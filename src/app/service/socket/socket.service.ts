bc4a358796c6eb63499194c3093e12709186e6fa
quanm200699 <maplestory2012vn@gmail.com>
2021-05-24 15:58:29 +0700

    add socket service done

---
+++ src/app/service/socket/socket.service.ts
@@ -0,0 +1,46 @@
+import {Injectable} from '@angular/core';
+import {environment} from '../../../environments/environment';
+import {Product} from '../../model/product';
+import {ProductService} from '../product.service';
+import * as Stomp from 'stompjs';
+import * as SockJS from 'sockjs-client';
+import {stringify} from '@angular/compiler/src/util';
+
+const API_URL = `${environment.apiUrl}`;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SocketService {
+  stompClient: any;
+  products: Product[] = [];
+
+  constructor(private productService: ProductService) {
+    this.getAllProduct();
+  }
+
+  getAllProduct() {
+    this.productService.getAll().subscribe(products => this.products = products);
+  }
+
+  connect() {
+    const ws = new SockJS(`${API_URL}/ws`);
+    this.stompClient = Stomp.over(ws);
+    this.stompClient.connect(() => {
+      this.stompClient.subscribe('/topic/products', data => {
+        const jsonData = JSON.parse(data);
+        this.products.push(jsonData);
+      });
+    });
+  }
+
+  disconnect() {
+    if (this.stompClient != null) {
+      this.stompClient.disconnect();
+    }
+  }
+
+  createProductUsingSocket(product) {
+    this.stompClient.send('/app/products', {}, stringify(product));
+  }
+}
